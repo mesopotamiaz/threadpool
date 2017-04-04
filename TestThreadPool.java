94ab9a675522c423a7697568c28baaa1a0d8534f
搬砖的小学生 <1020460933@qq.com>
2017-04-05 00:59:52 +0800

    Create TestThreadPool.java
    一个小的测试类

---
+++ TestThreadPool.java
@@ -0,0 +1,40 @@
+package threadpool;
+
+import org.junit.Test;
+
+/**
+ * 使用简单线程池和直接开启线程的差别
+ * @author Administrator
+ *
+ */
+public class TestThreadPool {
+	public class MyThread implements Runnable{
+		protected String name;
+		public MyThread(){
+		}
+		public MyThread(String name){
+			this.name=name;
+		}
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(100);
+				//System.out.print(name+" ");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	@Test
+	public void testThreadPool() throws InterruptedException {
+		long starttime=System.currentTimeMillis();
+		for(int i=0;i<1000;i++){
+			ThreadPool.getInstance().start(new MyThread("testThreadPool"+Integer.toString(i)));
+		}
+		
+		long endtime=System.currentTimeMillis();
+		System.out.println("testThreadPool"+": "+(endtime-starttime));
+		System.out.println("getCreatedThreadsCount:"+ThreadPool.getInstance().getCreatedThreadsCount());
+		Thread.sleep(1000);
+	}
+}
